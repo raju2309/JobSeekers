12b12119540f4e3b795d686979a72b2792d248a1
raju2309 <56440664+raju2309@users.noreply.github.com>
2022-03-22 16:30:40 +0530

    first commit.

---
+++ Maven_project_demo/src/main/java/Ed_Maven/demo_class.java
@@ -0,0 +1,14 @@
+package Ed_Maven;
+
+public class demo_class {
+	public String sample() {
+		return "abd";
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+
+	}
+
+}
